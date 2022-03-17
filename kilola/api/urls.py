60a1106cd3ed8ccdae23fae41316410a529208e6
Menas <50955041+portacode@users.noreply.github.com>
2022-03-17 17:49:48 +0400

    Add autogenerated swagger and redoc API documentation

--- kilola/api/urls.py
+++ kilola/api/urls.py
@@ -1,7 +1,32 @@
-from django.urls import path
+from django.urls import path, re_path
 from . import views
 from django.views.decorators.cache import cache_page
+from rest_framework.authtoken import views
+from django.views.generic import TemplateView
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Kilola API",
+      default_version='v1',
+      description="This is an auto generated documentation for the API",
+      terms_of_service="https://kilola-ui.pages.dev/terms",
+      contact=openapi.Contact(email="hi@menas.pro"),
+      license=openapi.License(name="MIT License"),
+   ),
+   public=True,
+   permission_classes=[permissions.AllowAny],
+)
 
 
 urlpatterns = [
+    path('login', views.obtain_auth_token),
+
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
